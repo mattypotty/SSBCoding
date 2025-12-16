4dc4ad607abcdc5cf7506006c6d8da74569213a3
Sarah Swiston <sarak.k.swiston@gmail.com>
2025-12-15 19:35:03 -0600

    script for full set of batch analyses

--- code/run_batch.sh
+++ code/run_batch.sh
@@ -1,9 +1,26 @@
-NUMBER_LIST=$(seq -w 001 001)
+NUMBER_LIST=$(seq -w 001 010)
 SSBTYPE_LIST=("SSB" "FSSB" "MSSB")
 HYPOTHESIS_LIST=("MI" "MGS" "SDT" "SOC" "RDM" "AM" "IUCN")
 VARIABLE_LIST=("NAC" "SPI" "TSP" "SDT" "RDM")
 
 USERANDOM="FALSE"
+
+for NUMBER in ${NUMBER_LIST[@]}
+do
+	for SSBTYPE in ${SSBTYPE_LIST[@]}
+	do
+		for HYPOTHESIS in ${HYPOTHESIS_LIST[@]}
+		do
+			source run_job.sh ${NUMBER} ${USERANDOM} "MCMCGLMM" ${SSBTYPE} ${HYPOTHESIS} "NONE"
+		done
+		for VARIABLE in ${VARIABLE_LIST[@]}
+		do
+			source run_job.sh ${NUMBER} ${USERANDOM} "PAGEL" ${SSBTYPE} "NONE" ${VARIABLE}
+		done
+	done
+done
+
+USERANDOM="TRUE"
 
 for NUMBER in ${NUMBER_LIST[@]}
 do
