ad8f05f270c3b37190ed950d8ee81f77f34e728f
Sarah Swiston <sarak.k.swiston@gmail.com>
2025-12-15 19:01:42 -0600

    setting up batch script

--- code/run_batch.sh
+++ code/run_batch.sh
@@ -1,13 +1,22 @@
 NUMBER_LIST=$(seq -w 001 001)
 SSBTYPE_LIST=("SSB" "FSSB" "MSSB")
-HYPOTHESIS_LIST=("MI" "MGS" "SDT" "RDM" "AM" "IUCN")
+HYPOTHESIS_LIST=("MI" "MGS" "SDT" "SOC" "RDM" "AM" "IUCN")
 VARIABLE_LIST=("NAC" "SPI" "TSP" "SDT" "RDM")
+
+USERANDOM="FALSE"
 
 for NUMBER in ${NUMBER_LIST[@]}
 do
 	for SSBTYPE in ${SSBTYPE_LIST[@]}
 	do
-		source run_job.sh ${NUMBER} "FALSE" "MCMCGLMM" ${SSBTYPE} "MI" "NONE"
+		for HYPOTHESIS in ${HYPOTHESIS_LIST[@]}
+		do
+			source run_job.sh ${NUMBER} ${USERANDOM} "MCMCGLMM" ${SSBTYPE} ${HYPOTHESIS} "NONE"
+		done
+		for VARIABLE in ${VARIABLE_LIST[@]}
+		do
+			source run_job.sh ${NUMBER} ${USERANDOM} "PAGEL" ${SSBTYPE} "NONE" ${VARIABLE}
+		done
 	done
 done
 
@@ -16,5 +25,5 @@ done
 # USERANDOM                 > "TRUE", "FALSE"
 # ANALYSIS                  > "NONE", "MCCTREE", "SPECMATS", "MCMCGLMM", "PAGEL"
 # SSBTYPE                   > "NONE", "SSB", "FSSB", "MSSB"
-# HYPOTHESIS (for MCMCglmm) > "NONE", "MI", "MGS", "SDT", "RDM", "AM", "IUCN"
+# HYPOTHESIS (for MCMCglmm) > "NONE", "MI", "MGS", "SDT", "SOC", "RDM", "AM", "IUCN"
 # VARIABLE (for Pagel)      > "None", "NAC", "SPI", "TSP", "SDT", "RDM"
