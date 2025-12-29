d55650c320ed67043f705f740c1d6eef12fdd875
Swiston <k.swiston@compute1-client-1.ris.wustl.edu>
2025-12-29 15:41:15 -0600

    SSE cluster scripts

---
+++ code/run_SSE.sh
@@ -0,0 +1,18 @@
+# Recording date and time of inference
+INF=$LSB_JOBNAME
+echo "Performing inference $INF"
+START=$( date '+%F_%H:%M:%S' )
+echo $START
+
+rb ./code/SSE.Rev $1
+
+# Recoding end date and time of inference
+echo "Finished inference $INF"
+END=$( date '+%F_%H:%M:%S' )
+echo $END
+
+T=$(printf '\t')
+echo "$INF$T$START$T$END" >> /storage1/fs1/michael.landis/Active/SSBCoding/joblogs/run_log.txt
+
+# OPTIONS:
+# DATASET > Afrotheria Artiodactyla_Aquatic Artiodactyla_Terrestrial Basal_Euarchontoglires Carnivora_Caniformia Carnivora_Feliformia Chiroptera_Pteropodiformes Chiroptera_Vespertilioniformes Lagomorpha Marsupalia Panperissodactyla Primates Rodentia_Hystricomorpha Rodentia_Sciuromorpha Rodentia_Supramyomorpha_Myomorphi Rodentia_Supramyomorpha_non-Myomorphi Xenarthra
---
+++ code/run_batch_SSE.sh
@@ -0,0 +1,46 @@
+NUMBER_LIST=$(seq -w 001 010)
+SSBTYPE_LIST=("SSB" "FSSB" "MSSB")
+HYPOTHESIS_LIST=("MI" "MGS" "SDT" "SOC" "RDM" "AM" "IUCN")
+VARIABLE_LIST=("NAC" "SPI" "TSP" "SDT" "RDM")
+
+USERANDOM="FALSE"
+
+for NUMBER in ${NUMBER_LIST[@]}
+do
+	for SSBTYPE in ${SSBTYPE_LIST[@]}
+	do
+		for HYPOTHESIS in ${HYPOTHESIS_LIST[@]}
+		do
+			source run_job.sh ${NUMBER} ${USERANDOM} "MCMCGLMM" ${SSBTYPE} ${HYPOTHESIS} "NONE"
+		done
+		for VARIABLE in ${VARIABLE_LIST[@]}
+		do
+			source run_job.sh ${NUMBER} ${USERANDOM} "PAGEL" ${SSBTYPE} "NONE" ${VARIABLE}
+		done
+	done
+done
+
+USERANDOM="TRUE"
+
+for NUMBER in ${NUMBER_LIST[@]}
+do
+	for SSBTYPE in ${SSBTYPE_LIST[@]}
+	do
+		for HYPOTHESIS in ${HYPOTHESIS_LIST[@]}
+		do
+			source run_job.sh ${NUMBER} ${USERANDOM} "MCMCGLMM" ${SSBTYPE} ${HYPOTHESIS} "NONE"
+		done
+		for VARIABLE in ${VARIABLE_LIST[@]}
+		do
+			source run_job.sh ${NUMBER} ${USERANDOM} "PAGEL" ${SSBTYPE} "NONE" ${VARIABLE}
+		done
+	done
+done
+
+# OPTIONS:
+# NUMBER                    > 001 : 010
+# USERANDOM                 > "TRUE", "FALSE"
+# ANALYSIS                  > "NONE", "MCCTREE", "SPECMATS", "MCMCGLMM", "PAGEL"
+# SSBTYPE                   > "NONE", "SSB", "FSSB", "MSSB"
+# HYPOTHESIS (for MCMCglmm) > "NONE", "MI", "MGS", "SDT", "SOC", "RDM", "AM", "IUCN"
+# VARIABLE (for Pagel)      > "None", "NAC", "SPI", "TSP", "SDT", "RDM"
