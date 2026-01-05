4922c4e79e4aac77a5986b89b1f8995324209fa7
Swiston <k.swiston@compute1-client-1.ris.wustl.edu>
2026-01-05 12:47:43 -0600

    initial SSE runs

--- code/run_SSE.sh
+++ code/run_SSE.sh
@@ -12,7 +12,7 @@ END=$( date '+%F_%H:%M:%S' )
 echo $END
 
 T=$(printf '\t')
-echo "$INF$T$START$T$END" >> /storage1/fs1/michael.landis/Active/SSBCoding/joblogs/run_log
+echo "$INF$T$START$T$END" >> /storage1/fs1/michael.landis/Active/SSBCoding/joblogs/run_log.txt
 
 # OPTIONS:
 # DATASET > Afrotheria Artiodactyla_Aquatic Artiodactyla_Terrestrial Basal_Euarchontoglires Carnivora_Caniformia Carnivora_Feliformia Chiroptera_Pteropodiformes Chiroptera_Vespertilioniformes Eulipotyphla Lagomorpha Marsupalia Panperissodactyla Primates Rodentia_Hystricomorpha Rodentia_Sciuromorpha Rodentia_Supramyomorpha_Myomorphi Rodentia_Supramyomorpha_non-Myomorphi Xenarthra
--- code/run_batch_SSE.sh
+++ code/run_batch_SSE.sh
@@ -1,5 +1,5 @@
-#DATASET_LIST=("Afrotheria" "Artiodactyla_Aquatic" "Artiodactyla_Terrestrial" "Basal_Euarchontoglires" "Carnivora_Caniformia" "Carnivora_Feliformia" "Chiroptera_Pteropodiformes" "Chiroptera_Vespertilioniformes" "Eulipotyphla" "Lagomorpha" "Marsupalia" "Panperissodactyla" "Primates" "Rodentia_Hystricomorpha" "Rodentia_Sciuromorpha" "Rodentia_Supramyomorpha_Myomorphi" "Rodentia_Supramyomorpha_non-Myomorphi" "Xenarthra")
-DATASET_LIST=("Primates")
+DATASET_LIST=("Afrotheria" "Artiodactyla_Aquatic" "Artiodactyla_Terrestrial" "Basal_Euarchontoglires" "Carnivora_Caniformia" "Carnivora_Feliformia" "Chiroptera_Pteropodiformes" "Chiroptera_Vespertilioniformes" "Eulipotyphla" "Lagomorpha" "Marsupalia" "Panperissodactyla" "Primates" "Rodentia_Hystricomorpha" "Rodentia_Sciuromorpha" "Rodentia_Supramyomorpha_Myomorphi" "Rodentia_Supramyomorpha_non-Myomorphi" "Xenarthra")
+#DATASET_LIST=("Primates")
 
 for DATASET in ${DATASET_LIST[@]}
 do
